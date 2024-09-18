38f0b92b2ff12d8c1696123f377b8148967691f1
DanielGar12 <garcia.daniel23256@gmail.com>
2024-09-18 12:09:37 -0500

    added icons

--- src/navigation/BottomTabNavigation/BottomTabNavigation.js
+++ src/navigation/BottomTabNavigation/BottomTabNavigation.js
@@ -4,23 +4,36 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import HomeScreen from '../../screens/HomeScreen';
 import SettingsScreen from '../../screens/SettingsScreen';
 import { NavigationContainer } from '@react-navigation/native';
-
+import Ionicons from 'react-native-vector-icons/Ionicons'
 const Tab = createBottomTabNavigator();
-
 const BottomTabNavigation = () => {
-  return (
-
-        <Tab.Navigator screenOptions={{
+    return (
+      
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+  
+              if (route.name === 'Home') {
+                iconName = focused ? 'home' : 'home-outline';
+              } else if (route.name === 'Settings') {
+                iconName = focused ? 'settings' : 'settings-outline';
+              }
+  
+              // Return the icon component
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
             tabBarActiveTintColor: 'blue',
             tabBarInactiveTintColor: 'grey',
-            tabBarLabelStyle: {paddingBottom: 10, fontSize: 15},
-            tabBarStyle: {padding: 10, height: 60 }
-            }}>
-            <Tab.Screen name='Home' component={HomeScreen}/>
-            <Tab.Screen name="Settings" component={SettingsScreen}/>
+            tabBarLabelStyle: { paddingBottom: 5, fontSize: 15 },
+            tabBarStyle: { padding: 10, height: 60 },
+            headerShown: false,
+          })}
+        >
+          <Tab.Screen name='Home' component={HomeScreen} />
+          <Tab.Screen name='Settings' component={SettingsScreen} />
         </Tab.Navigator>
-   
-  )
-}
 
+    );
+  };
 export default BottomTabNavigation;
--- src/screens/SettingsScreen/SettingsScreen.js
+++ src/screens/SettingsScreen/SettingsScreen.js
@@ -1,12 +1,34 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet} from 'react-native'
 import React from 'react'
+import CustomButton from '../../components/CustomButton'
+import { useNavigation } from '@react-navigation/native';
 
 const SettingsScreen = () => {
+    navigation= useNavigation();
+    const onRegisterPressed = () => {navigation.navigate('LogIn')}
   return (
-    <View>
-      <Text>SettingsScreen</Text>
+    <View style={styles.root}>
+      <Text style={styles.textStyle1}>Settings</Text>
+      <CustomButton
+      onPress={onRegisterPressed}
+      text={"Sign out"}
+      />
     </View>
   )
 }
+const styles = StyleSheet.create ({
+    
+    root: {
+        
+        padding: 40,
 
+    },
+
+    textStyle1: {
+        fontWeight: 'bold',
+        fontSize: 30
+    }
+
+
+})
 export default SettingsScreen;
