e4f38afbc97ee7d4d6aa817089d9b55b14cf3880
dug0004 <DanielGarcia20@my.unt.edu>
2024-09-19 16:24:32 -0500

    enabled android capabilities

--- src/components/CustomInput/CustomInput.js
+++ src/components/CustomInput/CustomInput.js
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderRadius: 5,
         paddingHorizontal: 10,
-        paddingVertical: 15,
+        paddingVertical: 10,
         marginVertical: 5,
         
     },
